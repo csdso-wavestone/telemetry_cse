1733f669aa24015869b5738ddbf063b62d21f7cf
Justin Facquet <justin.facquet@wavestone.com>
2026-07-07 03:01:59 +0200

    fix

--- includes/navbar.php
+++ includes/navbar.php
@@ -8,7 +8,6 @@
 	<div class="collapse navbar-collapse" id="navbarSupportedContent">
 		<ul class="navbar-nav mr-auto">
 			<li class="nav-item active"><a class="nav-link" href="/index.php">Home</a></li>
-			<li class="nav-item"><a class="nav-link" href="/dashboard.php">Dashboard</a></li>
 		<?php
 
 		if (isset($_SESSION['username'])){
