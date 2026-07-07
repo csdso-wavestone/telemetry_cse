f4824fa934b85b38497ee1a3a2a077f0775c5f21
Justin Facquet <justin.facquet@wavestone.com>
2026-07-07 02:48:57 +0200

    test

--- includes/logged.php
+++ includes/logged.php
@@ -2,7 +2,7 @@
     <!-- Hero Section -->
     <section id="hero" class="hero section">
         <div class="hero-bg">
-          <img src="<?php echo (isset($base_url)) ? $base_url : '.'; ?>/assets/img/Picture1.jpg" alt="">
+          <img src="/assets/img/Picture1.jpg" alt="">
         </div>
         <div class="container text-center">
           <div class="d-flex flex-column justify-content-center align-items-center">
