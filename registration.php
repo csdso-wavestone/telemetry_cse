b216b1dc1e1298ddcc00fb368941d08f2701b39e
Justin Facquet <justin.facquet@wavestone.com>
2026-07-07 03:17:04 +0200

    simplification

--- registration.php
+++ registration.php
@@ -10,11 +10,28 @@ if (isset ($_SESSION['username'])){
 	echo "Logged in with name '" . $_SESSION['username'] . "'. You can <a href='/logout.php'>logout</a>";
 }
 else{
-    include 'includes/registration.html';
 
 ?>
+<section id="hero" class="hero section">
+    <div class="hero-bg">
+      <img src="/assets/img/Picture1.jpg" alt="">
+    </div>
+        <div class="container-fluid loginouter">
+            <div class="card bg-light mb-3">
+                <div class="card-body">
+    <form action="/check_registration.php" method="POST">
+        Registration username:<br>
+        <input type="text" name="username"><br>
+        Registration password:<br>
+        <input type="password" name="pass"><br><br>
+        <input type="submit" class="btn btn-outline-primary" value="Submit">
+    </form>
+</div>
+</div>
+    </div> <!-- end of <div class="container-left"> -->
+</div>
 
-
+</section><!-- /Hero Section -->
 <?php
 
 include 'includes/footer.html';
