d3420f8e7dd4ec264b5f7e8168631b41670b5c9c
Antoine-Ws <antoine.colombier@wavestone.com>
2026-08-13 11:48:26 +0200

    Removing picture table from the sql init code

--- sql.sql
+++ sql.sql
@@ -10,14 +10,3 @@ CREATE TABLE `users` (
 `users_password` varchar(30) DEFAULT NULL,
 PRIMARY KEY (`users_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-
-
-CREATE TABLE `pictures` (
-`pictures_id` int(12) unsigned NOT NULL AUTO_INCREMENT,
-`pictures_name` varchar(50) DEFAULT NULL,
-`id_users` int(12) unsigned DEFAULT NULL,
-PRIMARY KEY (`pictures_id`),
-KEY `fk_pictures_users` (`id_users`),
-CONSTRAINT `fk_pictures_users` FOREIGN KEY (`id_users`) REFERENCES `users` (`users_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
-
